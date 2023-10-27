02142037673a8ca1554979727231bf73ed74299d
mliamsinclair <m.liam.sinclair@gmail.com>
2023-10-27 14:50:19 -0400

    JwtTokenUtil changes

--- src/main/java/com/pumppals/pumppalsapi/util/JwtTokenUtil.java
+++ src/main/java/com/pumppals/pumppalsapi/util/JwtTokenUtil.java
@@ -22,7 +22,7 @@ public class JwtTokenUtil implements Serializable {
 
 	public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
 
-	SecretKey key = Jwts.SIG.HS512.key().build();
+	private static SecretKey key = Jwts.SIG.HS512.key().build();
 
 	// retrieve username from jwt token
 	public String getUsernameFromToken(String token) {
