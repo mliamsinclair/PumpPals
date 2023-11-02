ae51b966033ea886dbac9644803264673f15e132
mliamsinclair <m.liam.sinclair@gmail.com>
2023-11-02 15:31:14 -0400

    working jwt implementation

--- src/main/java/com/pumppals/pumppalsapi/repository/UserRepository.java
+++ src/main/java/com/pumppals/pumppalsapi/repository/UserRepository.java
@@ -6,9 +6,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.pumppals.pumppalsapi.model.PPUser;
+import com.pumppals.pumppalsapi.model.UserInfo;
 
 @Repository
-public interface UserRepo extends MongoRepository<PPUser, ObjectId> {
-    Optional<PPUser> findByUsername(String username);
+public interface UserRepository extends MongoRepository<UserInfo, ObjectId> {
+    Optional<UserInfo> findByUsername(String username);
 }
